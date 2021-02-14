0a877154b1e67c122d8f99fb05c501a3345236d1
juarezer7c <juarezer7c@gmail.com>
2021-02-14 10:49:11 -0600

    Se sube configuracion inicial

---
+++ src/test/java/com/corpcapsula/mx/MsPuntoVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corpcapsula.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPuntoVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
